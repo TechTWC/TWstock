49e9196f7a76c4d7862ebce3c1bbc94e46b5405d
TechTWC <siencetech.twc@gmail.com>
2026-07-14 23:58:49 +0800

    feat: initialize Phase A1 engine package

---
+++ twstock_engine/__init__.py
@@ -0,0 +1,6 @@
+"""Experimental deterministic screening logic for TWStock Phase A1."""
+
+RELEASE_NAME = "Phase A1 Logic Sandbox v0.1"
+STRATEGY_NAME = "Fundamental_Valuation_Trend_Breakout_v1"
+
+__all__ = ["RELEASE_NAME", "STRATEGY_NAME"]
